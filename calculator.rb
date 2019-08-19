35f310e37d278a118a8e2225e32bf6fa01387147
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-08-19 17:15:19 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 2
+second_number = 1# Add your variables here
+sum = first_number + second_number
+difference = first_number - second_number 
+product = first_number * second_number
+quotient = first_number / second_number
